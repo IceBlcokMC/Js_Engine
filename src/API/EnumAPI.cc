87f3c36209a45a2de2efa8a4cc6ef8b9384944ef
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2024-12-19 23:12:09 +0800

    refactor: optimize enum registration logic

--- src/API/EnumAPI.cc
+++ src/API/EnumAPI.cc
@@ -6,23 +6,24 @@
 #include <unordered_map>
 
 #define REGISTER_ENUM_MACRO(ENUM, NAME)                                                                                \
-    if (enumMap.find(NAME) == enumMap.end()) {                                                                         \
-        for (auto [value, key] : magic_enum::enum_entries<ENUM>()) {                                                   \
-            enumMap[NAME][key] = (int)value;                                                                           \
-        }                                                                                                              \
+    for (auto [value, key] : magic_enum::enum_entries<ENUM>()) {                                                       \
+        enumMap[NAME][key] = (int)value;                                                                               \
     }
 
 namespace jse::EnumAPI {
 
 void RegisterEnum(ScriptEngine* engine) {
+    static Local<Value> enumMap = ([]() -> Local<Value> {
+        std::unordered_map<std::string, std::unordered_map<std::string_view, int>> enumMap;
 
-    static std::unordered_map<std::string, std::unordered_map<std::string_view, int>> enumMap;
+        REGISTER_ENUM_MACRO(endstone::PermissionDefault, "PermissionDefault");
+        REGISTER_ENUM_MACRO(endstone::PluginLoadOrder, "PluginLoadOrder");
+        REGISTER_ENUM_MACRO(endstone::Logger::Level, "LoggerLevel");
 
-    REGISTER_ENUM_MACRO(endstone::PermissionDefault, "PermissionDefault");
-    REGISTER_ENUM_MACRO(endstone::PluginLoadOrder, "PluginLoadOrder");
-    REGISTER_ENUM_MACRO(endstone::Logger::Level, "LoggerLevel");
+        return ConvertToScriptX(enumMap);
+    })();
 
-    engine->set("Enums", ConvertToScriptX(enumMap));
+    engine->set("Enums", enumMap);
 }
 
 #undef REGISTER_ENUM_MACRO
