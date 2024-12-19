990cd3e64c3d3d2736b25a90c062df2a13651ec1
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2024-12-19 21:15:51 +0800

    refactor: refactor declaration and test files

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="./Enum/PermissionDefault.d.ts"/>
+/// <reference path="./Enum/LoggerLevel.d.ts"/>
+/// <reference path="./Enum/PluginLoadOrder.d.ts"/>
+
+/// <reference path="./API/Logger.d.ts"/>
+/// <reference path="./API/Plugin.d.ts"/>
+/// <reference path="./API/JSE.d.ts"/>
+/// <reference path="./API/PluginDescription.d.ts"/>
