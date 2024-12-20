949b62171f404cf0df61e84ae70199d329615325
engsr6982 <pwxg286704@163.com>
2024-12-20 22:13:47 +0800

    update xmake

--- xmake.lua
+++ xmake.lua
@@ -9,7 +9,7 @@ add_requires(
     "microsoft-gsl 4.0.0",
     "nlohmann_json 3.11.3",
     "boost 1.85.0",
-    -- "glm 1.0.1",
+    "glm 1.0.1",
     "concurrentqueue 1.0.4"
 )
 add_requires("magic_enum 0.9.7")
@@ -44,7 +44,7 @@ target("Js_Engine")
         "microsoft-gsl",
         "nlohmann_json",
         "boost",
-        -- "glm",
+        "glm",
         "concurrentqueue"
     )
     add_packages("magic_enum")
