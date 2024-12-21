52f8f4dadfdfc7efb39a00c0e62449e116632104
engsr6982 <pwxg286704@163.com>
2024-12-21 21:19:33 +0800

    fix(linux): 更新Linux平台补丁链接

--- xmake.lua
+++ xmake.lua
@@ -12,7 +12,7 @@ package("endstone")
     add_patches("0.5.6", "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/cxx20.patch",
                         "547ae3d325b8deb68747179b6bc3aa8772ba4efe36263bf31f34be7a3aac2ceb")
     if (is_plat("linux")) then 
-        add_patches("0.5.6", "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/build-linux/patch/linux.patch",
+        add_patches("0.5.6", "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/linux.patch",
                         "1e7c6a961abf821803b42dcd43d1c88524caa8012e333b9e47ec6faa00037c74")   
     end
 
