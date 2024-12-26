1b3b02a6fd80dc9ad2360ee2d127934c947bd4a3
engsr6982 <pwxg286704@163.com>
2024-12-27 02:05:15 +0800

    feat: 调整Plugin类适配Node后端

--- src/Loader/JavaScriptPlugin.cc
+++ src/Loader/JavaScriptPlugin.cc
@@ -9,42 +9,41 @@
 #include <iostream>
 
 
-
 namespace jse {
 
 
 JavaScriptPlugin::~JavaScriptPlugin() {
-//     EngineManager::getInstance().destroyEngine(this->engineId_);
-// #ifdef DEBUG
-//     std::cout << "JavaScriptPlugin::~JavaScriptPlugin()  Plugin: " << this->getName() << std::endl;
-// #endif
+    NodeManager::getInstance().destroyEngine(this->engineId_);
+#ifdef DEBUG
+    std::cout << "JavaScriptPlugin::~JavaScriptPlugin()  Plugin: " << this->getName() << std::endl;
+#endif
 }
 
 void JavaScriptPlugin::onLoad() {
-    // auto        engine = EngineManager::getInstance().getEngine(this->engineId_);
-    // EngineScope scope(engine);
-    // try {
-    //     ENGINE_DATA()->callOnLoad();
-    // }
-    // CatchNotReturn;
+    auto        engine = NodeManager::getInstance().getEngine(this->engineId_);
+    EngineScope scope(engine->mEngine);
+    try {
+        ENGINE_DATA()->callOnLoad();
+    }
+    CatchNotReturn;
 }
 
 void JavaScriptPlugin::onEnable() {
-    // auto        engine = EngineManager::getInstance().getEngine(this->engineId_);
-    // EngineScope scope(engine);
-    // try {
-    //     ENGINE_DATA()->callOnEnable();
-    // }
-    // CatchNotReturn;
+    auto        engine = NodeManager::getInstance().getEngine(this->engineId_);
+    EngineScope scope(engine->mEngine);
+    try {
+        ENGINE_DATA()->callOnEnable();
+    }
+    CatchNotReturn;
 }
 
 void JavaScriptPlugin::onDisable() {
-    // auto        engine = EngineManager::getInstance().getEngine(this->engineId_);
-    // EngineScope scope(engine);
-    // try {
-    //     ENGINE_DATA()->callOnDisable();
-    // }
-    // CatchNotReturn;
+    auto        engine = NodeManager::getInstance().getEngine(this->engineId_);
+    EngineScope scope(engine->mEngine);
+    try {
+        ENGINE_DATA()->callOnDisable();
+    }
+    CatchNotReturn;
 }
 
 bool JavaScriptPlugin::onCommand(
@@ -52,21 +51,21 @@ bool JavaScriptPlugin::onCommand(
     const endstone::Command&        command,
     const std::vector<std::string>& args
 ) {
-    // auto        engine = EngineManager::getInstance().getEngine(this->engineId_);
-    // EngineScope scope(engine);
-    // try {
-    //     auto data = ENGINE_DATA();
-    //     auto obj  = data->mRegisterInfo.get();
-    //     if (obj.has("onCommand")) {
-    //         auto func = obj.get("onCommand");
-    //         if (func.isFunction()) {
-    //             // TODO: args
-    //             return func.asFunction().call({}, CommandSenderAPI::newCommandSenderAPI(&sender)).asBoolean().value();
-    //         }
-    //     }
-    //     Entry::getInstance()->getLogger().error("Plugin '{}' does not register onCommand function", data->mFileName);
-    // }
-    // CatchNotReturn;
+    auto        engine = NodeManager::getInstance().getEngine(this->engineId_);
+    EngineScope scope(engine->mEngine);
+    try {
+        auto data = ENGINE_DATA();
+        auto obj  = data->mRegisterInfo.get();
+        if (obj.has("onCommand")) {
+            auto func = obj.get("onCommand");
+            if (func.isFunction()) {
+                // TODO: args
+                return func.asFunction().call({}, CommandSenderAPI::newCommandSenderAPI(&sender)).asBoolean().value();
+            }
+        }
+        Entry::getInstance()->getLogger().error("Plugin '{}' does not register onCommand function", data->mFileName);
+    }
+    CatchNotReturn;
     return true;
 }
 
