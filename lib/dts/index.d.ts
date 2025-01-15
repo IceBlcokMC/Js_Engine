03994ca2a9667022c455cd4206daa8e32e6e646e
engsr6982 <pwxg286704@163.com>
2025-01-15 13:39:39 +0800

    chore: change floder name to lower case

--- lib/dts/index.d.ts
+++ lib/dts/index.d.ts
@@ -1,6 +1,6 @@
-/// <reference path="./Enum/PermissionDefault.d.ts"/>
-/// <reference path="./Enum/LoggerLevel.d.ts"/>
-/// <reference path="./Enum/PluginLoadOrder.d.ts"/>
+/// <reference path="./Enum1/PermissionDefault.d.ts"/>
+/// <reference path="./Enum1/LoggerLevel.d.ts"/>
+/// <reference path="./Enum1/PluginLoadOrder.d.ts"/>
 
 /// <reference path="./API/Actor/Actor.d.ts"/>
 /// <reference path="./API/Actor/Mob.d.ts"/>
